5bdbdd87afc7206c093d2ff3872eecad7648918f
xujinjin <xujinjin@58.com>
2023-10-26 17:03:26 +0800

    测试代码

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.lucas.lib-middleware"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 29
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+
+    api(project(mapOf("path" to ":lib-basecode")))
+
+}
---
+++ src/main/java/com/lucas/lib_middleware/test.java
@@ -0,0 +1,9 @@
+package com.lucas.lib_middleware;
+
+import com.lucas.lib_basecode.XBaseUtils;
+
+public class test {
+    public static void  goT(){
+
+    }
+}
